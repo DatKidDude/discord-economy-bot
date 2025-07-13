2bbf15ce9207608818da35db30b83b1a49fe5c4d
Craig Giddings <randomhero_345@hotmail.com>
2025-07-13 13:10:07 -0400

    Setup discord bot boilerplate

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN", "None")
+
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    if bot.user:
+        print(f"We are ready to go in, {bot.user.name}")
+    else:
+        print("Something went wrong went starting up the bot")
+
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server {member.name}")
+    
+
+# Ensure TOKEN is always a string to satisfy bot.run(),
+# since os.getenv() can return None if the env variable is missing.
+# Providing a default None string avoids linter/type checker warnings.
+try:
+    bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
+except discord.errors.LoginFailure as e:
+    print(f"A login error occurred: {e}")
+finally:
+    print("Closing application")
